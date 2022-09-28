a1c52c0142588dbcd87aea52e8a49194b5e36e45
Prg-maker <df3296709@gmail.com>
2022-09-28 14:33:17 -0300

    add img svg

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import { StyleSheet, Text, View , Image } from 'react-native';
+import LogoImg from './src/assets/Logo.svg'
 
 export default function App() {
   return (
    <View>
+     <LogoImg/>
 
+     <Text>dsadsadasdsad</Text>
    </View>
 
   );
