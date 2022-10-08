af1b5210159151866c0b3f1fbebcef2311d16c2a
Prg-maker <df3296709@gmail.com>
2022-10-08 13:02:02 -0300

    add header and status in Details

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,11 +1,24 @@
+import { CircleWavyCheck } from 'phosphor-react-native';
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import { HeaderSolicitation } from '../../components/HeaderSolicitation';
+import theme from '../../styles/theme';
 
 import { styles } from './styles';
 
 export function Details() {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <HeaderSolicitation/>
+      </View>
+
+      <View style={styles.status}>
+        <CircleWavyCheck  style={styles.icon} size={32} color={theme.colors.green[300]} />
+        <Text style={styles.textStatus}>
+          FINALIZADO
+        </Text>
+      </View>
 
     </View>
   );
--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -1,5 +1,29 @@
 import { StyleSheet } from 'react-native';
+import theme from '../../styles/theme';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex:1,
+    backgroundColor:theme.colors.gray[700],
+
+  },
+  header:{
+    backgroundColor:theme.colors.gray[600],
+    paddingBottom:35
+  },
+  status:{
+    backgroundColor:theme.colors.gray[500],
+    justifyContent:'center',
+    alignItems:'center',
+    paddingVertical:10,
+    flexDirection:'row'
+  },
+  icon:{
+    marginRight:14
+  },
+  textStatus:{
+    color:theme.colors.green[300],
+    fontSize:theme.fontSizes.sm,
+    fontFamily:theme.fonts.heading
+  }
 });
